83bfea41d72aeced71854dd93acf771616a18a34
szaboszinti <szabo.szintia@diak.szbi-pg.hu>
2026-01-29 10:44:02 +0100

    első és második feladat megoldás

---
+++ 2.js
@@ -0,0 +1,8 @@
+//2. Olvasd be egy téglalap két oldalának hosszát, majd írasd ki a kerületét és a területét!
+let a = prompt("Add meg a téglalap 'a' oldalának hosszát:" , 0);
+let b = prompt("Add meg a téglalap 'b' oldalának hosszát:" , 0);
+
+let k = 2 *(number(a) + Number(b));
+console.log(k);
+let t = Number(a) * Number(b);
+console.log(t);
---
+++ ujra.js
@@ -0,0 +1,5 @@
+//1. Írj programot, ami beolvassa a felhasználó nevét, majd köszön neki!
+let felh_nev = prompt("Kérem, adja meg a nevét: ", "pl.Jani");
+console.log(`Szia, ${felh_nev}!`);
+
+
